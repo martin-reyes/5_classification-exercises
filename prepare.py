f9747fb66df5083af6bb2725181c81f54d109dd7
Martin Reyes <martin.anthony.reyes@gmail.com>
2023-06-08 11:32:40 -0500

    tidy_data exercises done

--- prepare.py
+++ prepare.py
@@ -65,8 +65,7 @@ def prep_telco(telco=acquire.get_telco_data()):
 
 def split_data(df, test_size=.2, validate_size=.2, stratify_col=None, random_state=None):
     '''
-    take in a DataFrame and return train, validate, and test DataFrames;.
-    default size 
+    take in a DataFrame and return train, validate, and test DataFrames;
     return train, validate, test DataFrames.
     '''
     # no stratification
@@ -74,13 +73,16 @@ def split_data(df, test_size=.2, validate_size=.2, stratify_col=None, random_sta
         # split test data
         train_validate, test = train_test_split(df, test_size=test_size, random_state=random_state)
         # split validate data
-        train, validate = train_test_split(train_validate, test_size=validate_size, random_state=random_state)
+        train, validate = train_test_split(train_validate, test_size=validate_size/(1-test_size),
+                                                                           random_state=random_state)
     # stratify split
     else:
         # split test data
-        train_validate, test = train_test_split(df, test_size=test_size, random_state=random_state,
-                                                stratify = df[stratify_col])
+        train_validate, test = train_test_split(df, test_size=test_size,
+                                                random_state=random_state, stratify=df[stratify_col])
         # split validate data
-        train, validate = train_test_split(train_validate, test_size=validate_size, random_state=random_state,
-                                           stratify=train_validate[stratify_col])       
+        train, validate = train_test_split(train_validate, test_size=validate_size/(1-test_size),
+                                           random_state=random_state, stratify=train_validate[stratify_col])       
     return train, validate, test
+
+
